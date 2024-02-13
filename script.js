a679d4ee23600e800f8c505e863662b40a135d3f
Alex Le <140272444+RiceSupply@users.noreply.github.com>
2024-02-12 16:07:32 -0800

    added basic variables, started working on questions

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //HTML elements
+const startButton = document.getElementById("startbtn");
+const homePage = document.getElementById("homepage");
+const highScoreButton = document.getElementById("highscorebtn");
+
+const quizQuestions = [{
+  question: ""
+}]
