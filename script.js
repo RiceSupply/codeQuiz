171212cbdeb50aa1440c8393c93647efbcb517a3
Alex Le <140272444+RiceSupply@users.noreply.github.com>
2024-02-12 16:27:02 -0800

    Added more variables for future elements, added some questions and answers

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
 //HTML elements
-const startButton = document.getElementById("startbtn");
-const homePage = document.getElementById("homepage");
-const highScoreButton = document.getElementById("highscorebtn");
+const homePage = document.getElementById("homePage");
+const startButton = document.getElementById("startBtn");
+const quizPage = document.getElementById("quizPage");
+const highScorePage = document.getElementById("highScore{age");
+const highScoreButton = document.getElementById("highScoreBtn");
+const finalScore = document.getElementById("finalScore");
 
-const quizQuestions = [{
-  question: ""
-}]
+const buttonA = document.getElementById("A");
+const buttonB = document.getElementById("B");
+const buttonC = document.getElementById("C");
+const buttonD = document.getElementById("D");
+
+const quizQuestions = [
+  {
+    question: "What is the primary language used for styling?",
+    optionA : "CSS",
+    optionB : "HTML",
+    optionC : "JavaScript",
+    optionD : "Python",
+    correctAnswer: "A"
+  },
+
+  {
+    question: "What does WWW stand for?",
+    optionA: "World Web Widgets",
+    optionB: "World Wide Web",
+    optionC: "World Webpage Wonders",
+    optionD: "World Wide Want",
+    correctAnswer: "B",
+  },
+
+  {
+    question: "Where is localStorage stored?",
+    optionA: "Server",
+    optionB: "Database",
+    optionC: "Client side",
+    optionD: "Cloud",
+    correctAnswer: "C",
+  }
+]
